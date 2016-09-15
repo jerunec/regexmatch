0963a93943a207b0243335cfd3eabea5dede9c9a
jpcunanan <jpcunanan@192.168.56.1>
2016-09-15 08:43:22 +0800

    initial commit

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -5,20 +5,34 @@
  */
 package javaapplication1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author jpcunanan
  */
 public class JavaApplication1 {
 
-    public static boolean isTrue(String str){
-        return str.matches("true");
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public static boolean isTrue(String str){
+    return str.matches("true");
+    
+}
+public static boolean containsFalse(String str){
+    return str.matches(".*false.*");
+    
+}
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(isTrue("false"));
+        System.out.println(containsFalse("dsadasdasdsad"));
+      Scanner sc = new Scanner(new FileReader("input.txt"));
+
+while(sc.hasNext())
+{
+    System.out.println(isTrue(sc.nextLine()));
+    
+}
     }
     
 }
